aec4df741908fdac1cb2295fac886dd2c6bc3f17
Martin Podloucký <podlouckymartin@gmail.com>
2025-10-18 19:55:15 +0200

    Implement GameRow component and refactor HomePage to use it for displaying game details

---
+++ src/pages/GameRow/index.tsx
@@ -0,0 +1,60 @@
+import { JSX } from "react";
+import { ChessGame } from "../../db.js";
+
+interface Props {
+  game: ChessGame;
+}
+
+export const GameRow = ({ game }: Props): JSX.Element => {
+  return (
+    <div className="game-row">
+      <div className="col-players">
+        <div className="players-inline">
+          <div className="player-inline white-player">
+            <span className="player-color">♔</span>
+            <a href={`/player/${game.white.name}`} className="player-name">{game.white.name}</a>
+            <span className="player-rating">({game.white.rating})</span>
+            {game.ratingChange.white > 0 ? (
+              <span className="rating-change positive">+{game.ratingChange.white}</span>
+            ) : game.ratingChange.white < 0 ? (
+              <span className="rating-change negative">{game.ratingChange.white}</span>
+            ) : null}
+          </div>
+          <span className="vs-inline">vs</span>
+          <div className="player-inline black-player">
+            <span className="player-color">♚</span>
+            <a href={`/player/${game.black.name}`} className="player-name">{game.black.name}</a>
+            <span className="player-rating">({game.black.rating})</span>
+            {game.ratingChange.black > 0 ? (
+              <span className="rating-change positive">+{game.ratingChange.black}</span>
+            ) : game.ratingChange.black < 0 ? (
+              <span className="rating-change negative">{game.ratingChange.black}</span>
+            ) : null}
+          </div>
+        </div>
+      </div>
+
+      <div className="col-result">
+        <span className={`result-badge result-${game.result.replace('-', '')}`} title={`Ended by ${game.endingType}`}>
+          {game.result}
+        </span>
+        <div className="ending-type">{game.endingType}</div>
+      </div>
+
+      <div className="col-date">
+        <span className="game-date">{game.date}</span>
+      </div>
+
+      <div className="col-description">
+        <span className="game-description-inline">{game.description}</span>
+      </div>
+
+      <div className="col-actions">
+        <div className="game-actions-inline">
+          <a href={game.url} target="_blank" className="btn btn-secondary btn-sm">Lichess</a>
+          <a href={`/game/${game.id}`} className="btn btn-primary btn-sm">Details</a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import { JSX } from "react";
 import { Layout } from "../../components/Layout";
 import { ChessGame } from "../../db.js";
+import { GameRow } from "../GameRow";
 
 interface Props {
   totalGames: number;
@@ -25,57 +26,7 @@ export const HomePage = ({ totalGames, games }: Props): JSX.Element => {
             <div className="col-actions">Actions</div>
           </div>
 
-          {games.map((game) => (
-            <div className="game-row" key={game.id}>
-              <div className="col-players">
-                <div className="players-inline">
-                  <div className="player-inline white-player">
-                    <span className="player-color">♔</span>
-                    <a href={`/player/${game.white.name}`} className="player-name">{game.white.name}</a>
-                    <span className="player-rating">({game.white.rating})</span>
-                    {game.ratingChange.white > 0 ? (
-                      <span className="rating-change positive">+{game.ratingChange.white}</span>
-                    ) : game.ratingChange.white < 0 ? (
-                      <span className="rating-change negative">{game.ratingChange.white}</span>
-                    ) : null}
-                  </div>
-                  <span className="vs-inline">vs</span>
-                  <div className="player-inline black-player">
-                    <span className="player-color">♚</span>
-                    <a href={`/player/${game.black.name}`} className="player-name">{game.black.name}</a>
-                    <span className="player-rating">({game.black.rating})</span>
-                    {game.ratingChange.black > 0 ? (
-                      <span className="rating-change positive">+{game.ratingChange.black}</span>
-                    ) : game.ratingChange.black < 0 ? (
-                      <span className="rating-change negative">{game.ratingChange.black}</span>
-                    ) : null}
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-result">
-                <span className={`result-badge result-${game.result.replace('-', '')}`} title={`Ended by ${game.endingType}`}>
-                  {game.result}
-                </span>
-                <div className="ending-type">{game.endingType}</div>
-              </div>
-
-              <div className="col-date">
-                <span className="game-date">{game.date}</span>
-              </div>
-
-              <div className="col-description">
-                <span className="game-description-inline">{game.description}</span>
-              </div>
-
-              <div className="col-actions">
-                <div className="game-actions-inline">
-                  <a href={game.url} target="_blank" className="btn btn-secondary btn-sm">Lichess</a>
-                  <a href={`/game/${game.id}`} className="btn btn-primary btn-sm">Details</a>
-                </div>
-              </div>
-            </div>
-          ))}
+          {games.map((game) => <GameRow game={game} key={game.id} />)}
         </div>
 
         {games.length === 0 && (
