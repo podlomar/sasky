ccc08199a66bc92a01d3bb7c678126cc89517b30
Martin Podloucký <podlouckymartin@gmail.com>
2025-10-21 08:57:56 +0200

    Refactor deploy script to include SSH password prompt and cleanup function

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
-scp -r dist/* podlomar@podlomar.me:/var/www/sasky.podlomar.me/server
-scp package.json package-lock.json podlomar@podlomar.me:/var/www/sasky.podlomar.me
-ssh podlomar@podlomar.me "cd /var/www/sasky.podlomar.me/server && npm install --production"
+# Prompt (no echo) for password, store in a shell variable
+read -r -s -p "SSH password: " SSHPASS_INPUT
+echo
+
+# Make sure we clear the variable on exit or interrupt
+cleanup() {
+  unset SSHPASS
+  unset SSHPASS_INPUT
+}
+trap cleanup EXIT INT TERM
+
+export SSHPASS="$SSHPASS_INPUT"
+
+sshpass -e ssh podlomar@podlomar.me "rm -rf /var/www/sasky.podlomar.me/server/*"
+sshpass -e scp -r dist/* podlomar@podlomar.me:/var/www/sasky.podlomar.me/server
+sshpass -e scp package.json package-lock.json podlomar@podlomar.me:/var/www/sasky.podlomar.me
+sshpass -e ssh podlomar@podlomar.me "cd /var/www/sasky.podlomar.me/server && npm install --production"
