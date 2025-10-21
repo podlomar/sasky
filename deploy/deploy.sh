1cb56a2d8e30b25c853dfa9804adf71094748bb1
Martin Podloucký <podlouckymartin@gmail.com>
2025-10-21 08:19:28 +0200

    Refactor deployment scripts: remove ssh-agent setup from deploy.sh, add nginx configuration and systemd service file for the application

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Deploy the built files to the server
-# Setup ssh-agent to use fot all commands
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
 scp -r dist/* podlomar@podlomar.me:/var/www/sasky.podlomar.me/server
 scp package.json package-lock.json podlomar@podlomar.me:/var/www/sasky.podlomar.me
 ssh podlomar@podlomar.me "cd /var/www/sasky.podlomar.me/server && npm install --production"
