f9249b7a58170ac1872da12488d3b0a29c8678dc
Martin Podloucký <podlouckymartin@gmail.com>
2025-10-21 08:59:36 +0200

    Enhance deploy script with additional echo statements for better visibility during deployment process

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,7 +14,13 @@ trap cleanup EXIT INT TERM
 
 export SSHPASS="$SSHPASS_INPUT"
 
+echo "Deploying to podlomar.me..."
+echo "Cleaning remote server directory..."
 sshpass -e ssh podlomar@podlomar.me "rm -rf /var/www/sasky.podlomar.me/server/*"
+
+echo "Uploading new files..."
 sshpass -e scp -r dist/* podlomar@podlomar.me:/var/www/sasky.podlomar.me/server
 sshpass -e scp package.json package-lock.json podlomar@podlomar.me:/var/www/sasky.podlomar.me
+
+echo "Installing production dependencies on remote server..."
 sshpass -e ssh podlomar@podlomar.me "cd /var/www/sasky.podlomar.me/server && npm install --production"
