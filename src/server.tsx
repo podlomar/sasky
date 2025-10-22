ad8386b161a538765a0aaee6b0892366db96aa24
Martin Podloucký <podlouckymartin@gmail.com>
2025-10-22 14:30:16 +0200

    Log game and player loading information; comment out PGN example and related code

--- src/server.tsx
+++ src/server.tsx
@@ -22,27 +22,30 @@ const render = async (component: JSX.Element, res: express.Response) => {
 
 const games = await loadGames();
 const players = await loadPlayers();
+
+console.log(`Loaded ${games.length} games and ${players.length} players from the database.`);
+console.log('Recalculating player ratings...');
 await recalculateRatings(games, players);
 
-const pgn = `
-[White "Martin Podloucký"]
-[Black "Karel Beneš"]
-[TimeControl "8+10"]
-[Result "0-1"]
+// const pgn = `
+// [White "Martin Podloucký"]
+// [Black "Karel Beneš"]
+// [TimeControl "8+10"]
+// [Result "0-1"]
 
-1.e4 e5 2.Nf3 Nc6 3.Bb5 {This opening is called the Ruy Lopez.} 3...a6
-4.Ba4 Nf6 5.O-O Be7 6.Re1 b5 7.Bb3 d6 8.c3 O-O 9.h3 Nb8 10.d4 Nbd7
-11.c4 c6 12.cxb5 axb5 13.Nc3 Bb7 14.Bg5 b4 15.Nb1 h6 16.Bh4 c5 17.dxe5
-Nxe4 18.Bxe7 Qxe7 19.exd6 Qf6 20.Nbd2 Nxd6 21.Nc4 Nxc4 22.Bxc4 Nb6
-23.Ne5 Rae8 24.Bxf7+ Rxf7 25.Nxf7 Rxe1+ 26.Qxe1 Kxf7 27.Qe3 Qg5 28.Qxg5
-hxg5 29.b3 Ke6 30.a3 Kd6 31.axb4 cxb4 32.Ra5 Nd5 33.f3 Bc8 34.Kf2 Bf5
-35.Ra7 g6 36.Ra6+ Kc5 37.Ke1 Nf4 38.g3 Nxh3 39.Kd2 Kb5 40.Rd6 Kc5 41.Ra6
-Nf2 42.g4 Bd3 43.Re6 1/2-1/2
-`;
+// 1.e4 e5 2.Nf3 Nc6 3.Bb5 {This opening is called the Ruy Lopez.} 3...a6
+// 4.Ba4 Nf6 5.O-O Be7 6.Re1 b5 7.Bb3 d6 8.c3 O-O 9.h3 Nb8 10.d4 Nbd7
+// 11.c4 c6 12.cxb5 axb5 13.Nc3 Bb7 14.Bg5 b4 15.Nb1 h6 16.Bh4 c5 17.dxe5
+// Nxe4 18.Bxe7 Qxe7 19.exd6 Qf6 20.Nbd2 Nxd6 21.Nc4 Nxc4 22.Bxc4 Nb6
+// 23.Ne5 Rae8 24.Bxf7+ Rxf7 25.Nxf7 Rxe1+ 26.Qxe1 Kxf7 27.Qe3 Qg5 28.Qxg5
+// hxg5 29.b3 Ke6 30.a3 Kd6 31.axb4 cxb4 32.Ra5 Nd5 33.f3 Bc8 34.Kf2 Bf5
+// 35.Ra7 g6 36.Ra6+ Kc5 37.Ke1 Nf4 38.g3 Nxh3 39.Kd2 Kb5 40.Rd6 Kc5 41.Ra6
+// Nf2 42.g4 Bd3 43.Re6 1/2-1/2
+// `;
 
-const purifiedPgn = purifyPgn(pgn);
-// const url = await postOnLichess(purifiedPgn);
-// console.log(`Game posted to Lichess: ${url}`);
+// const purifiedPgn = purifyPgn(pgn);
+// // const url = await postOnLichess(purifiedPgn);
+// // console.log(`Game posted to Lichess: ${url}`);
 
 app.get('/', async (req: Request, res: Response) => {
   const games = await loadGames();
