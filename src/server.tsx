ac8e6c9db7f65a5e85e18f9bdddd46651e60ea5a
Martin Podloucký <podlouckymartin@gmail.com>
2026-01-08 20:43:54 +0100

    Fix broken server.tsx

--- src/server.tsx
+++ src/server.tsx
@@ -4,7 +4,6 @@ import { prerenderToNodeStream } from 'react-dom/static';
 import { HomePage } from './pages/HomePage/index.js';
 import { EnterGamePage } from './pages/EnterGamePage/index.js';
 import { PlayersPage } from './pages/PlayersPage/index.js';
-import { GamesTable } from './pages/HomePage/components/GamesTable/index.js';
 import { loadGames, loadPlayers, saveGame, recalculateRatings, getPlayerByName, ChessGame } from './db.js';
 import { purifyPgn, postOnLichess } from './pgn.js';
 
@@ -28,14 +27,29 @@ console.log(`Loaded ${games.length} games and ${players.length} players from the
 console.log('Recalculating player ratings...');
 await recalculateRatings(games, players);
 
+// const pgn = `
+// [White "Martin Podloucký"]
+// [Black "Karel Beneš"]
+// [TimeControl "8+10"]
+// [Result "0-1"]
+
+// 1.e4 e5 2.Nf3 Nc6 3.Bb5 {This opening is called the Ruy Lopez.} 3...a6
+// 4.Ba4 Nf6 5.O-O Be7 6.Re1 b5 7.Bb3 d6 8.c3 O-O 9.h3 Nb8 10.d4 Nbd7
+// 11.c4 c6 12.cxb5 axb5 13.Nc3 Bb7 14.Bg5 b4 15.Nb1 h6 16.Bh4 c5 17.dxe5
+// Nxe4 18.Bxe7 Qxe7 19.exd6 Qf6 20.Nbd2 Nxd6 21.Nc4 Nxc4 22.Bxc4 Nb6
+// 23.Ne5 Rae8 24.Bxf7+ Rxf7 25.Nxf7 Rxe1+ 26.Qxe1 Kxf7 27.Qe3 Qg5 28.Qxg5
+// hxg5 29.b3 Ke6 30.a3 Kd6 31.axb4 cxb4 32.Ra5 Nd5 33.f3 Bc8 34.Kf2 Bf5
+// 35.Ra7 g6 36.Ra6+ Kc5 37.Ke1 Nf4 38.g3 Nxh3 39.Kd2 Kb5 40.Rd6 Kc5 41.Ra6
+// Nf2 42.g4 Bd3 43.Re6 1/2-1/2
+// `;
+
+// const purifiedPgn = purifyPgn(pgn);
+// // const url = await postOnLichess(purifiedPgn);
+// // console.log(`Game posted to Lichess: ${url}`);
+
 app.get('/', async (req: Request, res: Response) => {
   const games = await loadGames();
-  const rows = games.map(game => ({
-    ...game,
-    canDelete: true,
-  }));
-
-  render(<HomePage totalGames={games.length} gamesData={rows} />, res);
+  render(<HomePage totalGames={games.length} games={games} />, res);
 });
 
 app.get('/enter', async (req: Request, res: Response) => {
@@ -134,33 +148,6 @@ app.post('/enter', async (req: Request, res: Response) => {
   }
 });
 
-app.delete('/game/:id', async (req: Request, res: Response) => {
-  try {
-    const gameId = req.params.id;
-    const games = await loadGames();
-    const gameIndex = games.findIndex(game => game.id === gameId);
-
-    if (gameIndex === -1) {
-      res.status(404).send('Game not found');
-      return;
-    }
-
-    games.splice(gameIndex, 1);
-    const rows = games.map(game => ({
-      ...game,
-      canDelete: true,
-    }));
-
-    await recalculateRatings(games, await loadPlayers());
-    render(<GamesTable gamesData={rows} />, res);
-  }
-  catch (error) {
-    console.error('Error deleting game:', error);
-    res.status(500).send('Error deleting game. Please try again.');
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
